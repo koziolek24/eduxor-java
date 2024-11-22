6235c4fe3747d380d56ba36140b827501ce8ec47
mkozlow6 <01187241@pw.edu.pl>
2024-11-22 21:03:13 +0100

    initialized project

---
+++ eduxor/backend/src/main/java/org/example/eduxor/EduxorApplication.java
@@ -0,0 +1,13 @@
+package org.example.eduxor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduxorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduxorApplication.class, args);
+    }
+
+}
---
+++ eduxor/backend/src/test/java/org/example/eduxor/EduxorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eduxor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduxorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
